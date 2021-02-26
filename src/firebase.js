16baa0257528177e0389e4c88c46b6f5b6242dcd
sheikoodally <44279883+sheikoodally@users.noreply.github.com>
2021-02-26 10:55:56 -0500

    calling api and frontend banner

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1g3eO4_gKH_B-4XCTmJLdYcSEmJC2n6g",
+  authDomain: "netflix-clone-so.firebaseapp.com",
+  projectId: "netflix-clone-so",
+  storageBucket: "netflix-clone-so.appspot.com",
+  messagingSenderId: "1058060383149",
+  appId: "1:1058060383149:web:84000e05cb1bbff6faa724"
+};
